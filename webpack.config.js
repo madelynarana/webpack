464708f27b58ab0b3b706cf632854b95381f1f00
Madelyn Arana <madelynarana@gmail.com>
2022-06-13 04:17:35 -0600

    :zap: Update.

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
 
     module: {
         rules: [
+             {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                ],
+            },
             {
                 test: /\.(s[ac]|c)ss$/i,
                 use: [{
