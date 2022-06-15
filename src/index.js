0ec0389de192322317d07925167a49a520793a37
Madelyn Arana <madelynarana@gmail.com>
2022-06-14 22:52:11 -0600

    :zap: Update template.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './assets/styles/index.scss';
+import licence from './assets/font/Lato/ofl.txt'; // Licence Lato.
 
-import rick from './assets/img/rick.gif'
+import rick from './assets/img/rick.gif';
 
 import { hello } from './assets/js/component';
 
 
-const img = document.createElement("img");
+const img = document.createElement('img');
 img.src =  rick;
 
 document.body.appendChild(img);
