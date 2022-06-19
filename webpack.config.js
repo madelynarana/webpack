22a2e902b1446505040acbd4482740622514d1ab
Madelyn Arana <madelynarana@gmail.com>
2022-06-18 18:18:37 -0600

    :purple_heart: Finish.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
-const MiniCssExtractPlugin   = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin      = require('html-webpack-plugin');
-const glob = require('glob');
-const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
+const glob                 = require('glob');
+const path                 = require('path');
 
 const devMode = process.env.NODE_ENV !== 'production'; // Development mode
 
+const entry = {};
 
-function getEntry() {
-    const entry = {};
+const getEntry = () => {
+    const filePaths = glob.sync('src/pages/*'); // Directory to collect input files.
 
-   //Directorio para recopilar archivos de entrada
-    const filePaths = glob.sync('src/pages/*');
-
-    // El nombre del directorio, que es el nombre del archivo empaquetado
-    filePaths.forEach(filePath => {
+    filePaths.forEach(filePath => { // Name of the directory, which is  name of package file.
         const filename = path.basename(filePath);
         entry[filename] = `./${filePath}/script.js`;
-      });
+    });
     return entry;
-  }
+}
 
+const getEntryHtmlPlugins = () => {
+    return Object.keys(entry).map(filename => {
+        return new HtmlWebpackPlugin({
+            template: `./src/pages/${filename}/index.html`,
+            filename: filename === "index" ? `index.html` : `${filename}/index.html`,
+            chunks: [`${filename}`],
+        });
+    });
+};
 
 
 module.exports = {
@@ -27,19 +33,10 @@ module.exports = {
     mode: devMode ? 'development' : 'production',
 
     entry: getEntry(),
-    // entry: {
-    //     index: {
-    //         import: './src/pages/index/script.js',
-    //         filename: 'index/script.js',
-    //     },
-    //     about: {
-    //         import: './src/pages/about/script.js',
-    //         filename: 'about/script.js',
-    //     },
-    // },
+
     output: {
         filename: "[name]/script.js",
-        assetModuleFilename: devMode ? 'assets/img/[name][ext]': 'img/[hash][ext]',
+        assetModuleFilename: devMode ? 'assets/img/[name][ext]' : 'img/[hash][ext]',
         clean: true,
     },
 
@@ -58,7 +55,7 @@ module.exports = {
                 use: [{
                     loader: MiniCssExtractPlugin.loader,
                     options: {
-                        publicPath:    '../'
+                        publicPath: '../'
                     },
                 },
                     'css-loader',
@@ -75,8 +72,8 @@ module.exports = {
             {
                 test: /.(ttf|otf|txt|eot|woff(2)?)(\?[a-z0-9]+)?$/,
                 type: 'asset/resource',
-                generator : {
-                    filename : devMode ? 'assets/font/[name][ext][query]':'font/[name][ext][query]',
+                generator: {
+                    filename: devMode ? 'assets/font/[name][ext][query]' : 'font/[name][ext][query]',
                 }
             },
 
@@ -92,20 +89,9 @@ module.exports = {
 
     plugins: [
         new MiniCssExtractPlugin({
-            filename:  '[name]/style.css',   
+            filename: '[name]/style.css',
         }),
-
-        new HtmlWebpackPlugin({
-            template: './src/pages/index/index.html',
-            chunks: ['index'],
-            filename: 'index.html'
-        
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/about/index.html',
-            chunks: ['about'],
-            filename: 'about/index.html'
-        }),
+        ...getEntryHtmlPlugins(),
     ],
 
     target: devMode ? 'web' : 'browserslist',
